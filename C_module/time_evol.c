f0fa91726a351428e9d03ec75f870ef47333e16b
fmayo96 <francomayo96@gmail.com>
2023-07-11 23:37:49 -0300

    Update time_evol.c

--- C_module/time_evol.c
+++ C_module/time_evol.c
@@ -64,7 +64,7 @@ void RK4_step(double complex *state, double dt, double temp, double j_0_3, doubl
     free(K4);
 }
 
-void Diff(double complex *diff_state, double complex*state, double temp, double j_0_3, double j_0_4){
+void Diff(double complex *diff_state, double complex *state, double temp, double j_0_3, double j_0_4){
     int i, j;
     double Rabi4;
     double *ks, *ws;
