2a4fd76569b04c369aa688909c93279f28656181
Franco Mayo <francomayo96@gmail.com>
2023-07-21 10:38:03 -0300

    fix Cooling-Yb/steady_state

--- Cooling-Yb/steady_state.py
+++ Cooling-Yb/steady_state.py
@@ -40,12 +40,12 @@ def Steady_state(T, j_0_3, j_0_4):
     M[3,0],M[3,1],M[3,2],M[3,3],M[3,10],M[3,11] = -gamma,-gamma, gamma_pl_3 - gamma, -(gamma_mn_3 + gamma), 1j*Omega_4,-1j*Omega_4
     M[4,4],M[4,5],M[4,8],M[4,9],M[4,10],M[4,11] = -4*gamma-gamma_pl_5,gamma_mn_5,-1j*Omega_3,1j*Omega_3,-1j*Omega_4,1j*Omega_4
     M[5,0],M[5,1],M[5,2],M[5,3],M[5,4],M[5,5] = -gamma_mn_6,-gamma_mn_6,-gamma_mn_6,-gamma_mn_6,gamma_pl_5 - gamma_mn_6, -(gamma_mn_5 + gamma_mn_6 + gamma_pl_6 +4*gamma)
-    M[6,6],M[6,8],M[6,11] = -1j*(E2-E3)/hbar-0.5*(gamma_mn_2+gamma_mn_3+gamma_pl_3),1j*Omega_4,-1j*Omega_3
-    M[7,7],M[7,9],M[7,10] = -1j*(E3-E2)/hbar-0.5*(gamma_mn_2+gamma_mn_3+gamma_pl_3),-1j*Omega_4,1j*Omega_3
-    M[8,2],M[8,4],M[8,6],M[8,8] = 1j*Omega_3,-1j*Omega_3,1j*Omega_4,-1j*(E2-E4)/hbar-0.5*(gamma_mn_2+gamma_pl_3+gamma_pl_5)
-    M[9,2],M[9,4],M[9,7],M[9,9] = -1j*Omega_3,1j*Omega_3,-1j*Omega_4,1j*(E2-E4)/hbar-0.5*(gamma_mn_2+gamma_pl_3+gamma_pl_5)
-    M[10,3],M[10,4],M[10,7],M[10,10] = 1j*Omega_4,-1j*Omega_4,1j*Omega_3,-1j*(E3-E4)/hbar-0.5*(gamma_mn_3-gamma_pl_5)
-    M[11,3],M[11,4],M[11,6],M[11,11] = -1j*Omega_4,1j*Omega_4,-1j*Omega_3,1j*(E3-E4)/hbar-0.5*(gamma_mn_3-gamma_pl_5)
+    M[6,6],M[6,8],M[6,11] = -1j*(E2-E3)-0.5*(gamma_mn_2+gamma_mn_3+gamma_pl_3),1j*Omega_4,-1j*Omega_3
+    M[7,7],M[7,9],M[7,10] = -1j*(E3-E2)-0.5*(gamma_mn_2+gamma_mn_3+gamma_pl_3),-1j*Omega_4,1j*Omega_3
+    M[8,2],M[8,4],M[8,6],M[8,8] = 1j*Omega_3,-1j*Omega_3,1j*Omega_4,-1j*(E2-E4)-0.5*(gamma_mn_2+gamma_pl_3+gamma_pl_5)
+    M[9,2],M[9,4],M[9,7],M[9,9] = -1j*Omega_3,1j*Omega_3,-1j*Omega_4,1j*(E2-E4)-0.5*(gamma_mn_2+gamma_pl_3+gamma_pl_5)
+    M[10,3],M[10,4],M[10,7],M[10,10] = 1j*Omega_4,-1j*Omega_4,1j*Omega_3,-1j*(E3-E4)-0.5*(gamma_mn_3-gamma_pl_5)
+    M[11,3],M[11,4],M[11,6],M[11,11] = -1j*Omega_4,1j*Omega_4,-1j*Omega_3,1j*(E3-E4)-0.5*(gamma_mn_3-gamma_pl_5)
 
 
     rho_ss = scipy.linalg.solve(M,b)
